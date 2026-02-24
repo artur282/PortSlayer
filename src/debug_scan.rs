6f60fa20d65535c10bcae4c0518d35cdac0cc3ed
artur282 <luisarturocruz57@gmail.com>
2026-02-24 16:12:12 -0400

    sadasd

--- src/debug_scan.rs
+++
@@ -1,8 +0,0 @@
-mod port_scanner;
-
-fn main() {
-    let ports = port_scanner::scan_open_ports();
-    for p in ports {
-        println!("{}", p);
-    }
-}
--- test_ss.rs
+++
@@ -1,5 +0,0 @@
-use std::process::Command;
-fn main() {
-    let output = Command::new("ss").arg("-tlnpH").output().unwrap();
-    println!("{}", String::from_utf8(output.stdout).unwrap());
-}
